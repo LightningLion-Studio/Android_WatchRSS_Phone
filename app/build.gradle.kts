59ca2b749b940c4517c17b270567e45994248b82
gfhdhytghd <3391146750@qq.com>
2026-08-19 15:46:41 +0800

    更新 release 1.2.2-15 版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.lightningstudio.watchrss.phone"
         minSdk = 30
         targetSdk = 36
-        versionCode = 44
-        versionName = "1.2.2-14"
+        versionCode = 45
+        versionName = "1.2.2-15"
         buildConfigField(
             "String",
             "WATCHRSS_PRODUCTION_BACKEND_BASE_URL",
